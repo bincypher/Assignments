e934831457a8d2d95b74c9f8b945bce6c5b52076
bincypher <binayak.bidyasagar99@gmail.com>
2025-10-03 08:05:28 +0530

    codes

--- javaScript/javaScript1/first.js
+++ javaScript/javaScript1/first.js
@@ -2,3 +2,17 @@ console.log("Hello World");
 console.log("Welcome to JavaScript");
 
 // REPL:Read–Eval–Print Loop.
+
+let c = 2/0;
+console.log(c); // Infinity
+
+let d = -2/0;
+console.log(d); // -Infinity
+
+let e = "abc"/2;
+console.log(e); // NaN
+
+let musk = 500_000_000_000;
+let ambani = 400_000_000_000;
+musk = musk + 100_000_000_000;
+console.log(musk + ambani);
